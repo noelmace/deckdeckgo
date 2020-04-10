9e9578e4ec0b693d71ea3100e355ce19bf036a37
Restyled.io <commits@restyled.io>
2020-04-10 17:10:21 +0000

    Restyled by clang-format

--- backend/src/socket-io-remote.js
+++ backend/src/socket-io-remote.js
@@ -2,7 +2,7 @@ let rooms = [];
 
 module.exports = server => {
   const socketIO = require("socket.io").listen(server, {
-    transports: ["websocket", "xhr-polling"]
+    transports : [ "websocket", "xhr-polling" ]
   });
 
   socketIO.set("origins", "*:*");
@@ -10,9 +10,8 @@ module.exports = server => {
   console.log("\x1b[36m%s\x1b[0m", "[DeckDeckGo]", "Socket listening. Path: /");
 
   socketIO.sockets.on("connection", socket => {
-    socket.on("rooms", async () => {
-      await emitRooms(socketIO, socket, false);
-    });
+    socket.on("rooms",
+              async () => { await emitRooms(socketIO, socket, false); });
 
     socket.on("join", async req => {
       if (req) {
@@ -25,12 +24,10 @@ module.exports = server => {
           const currentRoom = await findRoom(req.room);
 
           if (!currentRoom) {
-            rooms.push({
-              name: req.room,
-              connected: false
-            });
+            rooms.push({name : req.room, connected : false});
           } else {
-            // If already existing, reset connected as we might switch off/on in the deck
+            // If already existing, reset connected as we might switch off/on in
+            // the deck
             currentRoom.connected = false;
           }
 
@@ -46,18 +43,16 @@ module.exports = server => {
     });
 
     socket.on("signal", req => {
-      socket.broadcast.to(req.room).emit("signaling_message", {
-        type: req.type,
-        message: req.message,
-        fromSocketId: socket.id
-      });
+      socket.broadcast.to(req.room).emit(
+          "signaling_message",
+          {type : req.type, message : req.message, fromSocketId : socket.id});
     });
 
     socket.on("start", req => {
       if (req && req.toSocketId) {
         socketIO.to(`${req.toSocketId}`).emit("signaling_message", {
-          type: req.type,
-          message: req.message
+          type : req.type,
+          message : req.message
         });
       }
     });
@@ -81,14 +76,14 @@ function filterActiveRooms(socketIO) {
     if (rooms && rooms.length > 0) {
       rooms.forEach(room => {
         const activeRoom = socketIO.sockets.adapter.rooms
-          ? socketIO.sockets.adapter.rooms[room.name]
-          : null;
+                               ? socketIO.sockets.adapter.rooms[room.name]
+                               : null;
 
         if (activeRoom) {
           results.push({
-            room: room.name,
-            clients: activeRoom.length,
-            connected: room.connected
+            room : room.name,
+            clients : activeRoom.length,
+            connected : room.connected
           });
         }
       });
@@ -102,7 +97,8 @@ function filterActiveRooms(socketIO) {
  * activeRooms = [{
  *      room: string, // room name
  *      clients: number // 1 for app or deck, 2 if both are connected
- *      connected: boolean // the connection between remote and deck for this room is established?
+ *      connected: boolean // the connection between remote and deck for this
+ * room is established?
  * }]
  */
 function emitRooms(socketIO, socket, broadcast) {
@@ -110,13 +106,9 @@ function emitRooms(socketIO, socket, broadcast) {
     const activeRooms = await filterActiveRooms(socketIO);
 
     if (broadcast) {
-      socket.broadcast.emit("active_rooms", {
-        rooms: activeRooms
-      });
+      socket.broadcast.emit("active_rooms", {rooms : activeRooms});
     } else {
-      socket.emit("active_rooms", {
-        rooms: activeRooms
-      });
+      socket.emit("active_rooms", {rooms : activeRooms});
     }
 
     resolve();
@@ -130,9 +122,8 @@ function findRoom(roomName) {
       return;
     }
 
-    const room = rooms.find(filteredRoom => {
-      return filteredRoom.name === roomName;
-    });
+    const room =
+        rooms.find(filteredRoom => { return filteredRoom.name === roomName; });
 
     resolve(room);
   });
